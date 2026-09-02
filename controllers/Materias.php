c08f7e5c14e39ac3b6e38681752df38ff2cbe9b1
Gabi <gabrielalejandrojara4@gmail.com>
2026-09-02 12:01:35 -0300

    creacion de carpetas y archivos para aplicar MVC

---
+++ controllers/Materias.php
@@ -0,0 +1,35 @@
+<?php
+
+class Materias{
+    private $conn;
+
+    public function __construct($conexion){
+        $this->conn = $conexion;
+    }
+    //Traemos todas las materias para mostrarlas
+    public function obtenerTodas(){
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+?>
