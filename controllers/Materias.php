aae9376a7000498ef29bb8ab0077037f09939209
Gabi <gabrielalejandrojara4@gmail.com>
2026-09-02 12:51:46 -0300

    Modelo materias listo

--- controllers/Materias.php
+++ controllers/Materias.php
@@ -8,7 +8,41 @@ class Materias{
     }
     //Traemos todas las materias para mostrarlas
     public function obtenerTodas(){
-        
+        $resultado = this->conn->query("SELECT * FROM materias");
+        $materias = [];
+        while($fila = $resultado->fetch_assoc()){
+            $materias[] = $fila; //Agregamos materia en el array, cada elemento es una fila.
+        }
+        return $materias; 
+    }
+
+    //Para cuando editemos una materia, traemos sus datos.
+    public function obtenerPorId($id){
+        $stmt = $this->conn->prepare("SELECT * FROM materias WHERE idMateria = ?");
+        $stmt->bindParam("i", $id);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        return $resultado->fetch_assoc();
+    }
+    //Actualizamos la materia
+    public function actualizar($id, $nombre, $anio){
+        $stmt = $this->conn->prepare("UPDATE materias SET mat_nombre = ?, mat_anio = ? WHERE idMateria = ?");
+        $stmt->bindParam("sii", $id, $nombre, $anio);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        return $resultado->fetch_assoc();
+    }
+    //Creamos una materia
+    public function crear($nombre, $anio, $idEstado){
+        $stmt = $this->conn->prepare("INSERT INTO materias (mat_nombre, mat_anio, idEstado) VALUES (?,?,?)");
+        $stmt->bindParam("sii", $nombre, $anio, $idEstado);
+        return $stmt->execute();
+    }
+    //Eliminamos una materia
+    public function eliminar($id){
+        $stmt = $this->conn->prepare("DELETE FROM materias WHERE idMateria = ?");
+        $stmt->bindParam("i", $id);
+        return $stmt->execute();
     }
 
 
